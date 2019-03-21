4865fd88ad4b6b55941fe32428abc3451fcd6f88
syun07 <sarahyun7@gmail.com>
2019-03-21 11:32:46 -0400

    post req to create wavelengths works

--- app/controllers/tastes_controller.rb
+++ app/controllers/tastes_controller.rb
@@ -7,30 +7,41 @@ class TastesController < ApplicationController
 		@genre = request.headers['genre']
 
 		@result = 
-		RestClient.get("https://tastedive.com/api/similar?k=332551-SchoolPr-K9OLOPIS&info=1&q=#{@query}&type=#{@genre}")
+		RestClient.get("https://tastedive.com/api/similar?k=332551-SchoolPr-3QM2JZAF&info=1&q=#{@query}&type=#{@genre}")
 		
 		@api_data = JSON.parse(@result.body)
 
 		render json: @api_data
 	end
 
-  def index
-		@tastes = Tastes.all
+	def index
+		@tastes = Taste.all
 		render json: @tastes, status: :ok
 	end
 
 	def create
-		if @tastes.save
-			render json: @tastes, status: :created
+		@taste = Taste.find_or_create_by!(name: tastes_params[:name], type: tastes_params[:type], teaser: tastes_params[:teaser], wUrl: tastes_params[:wUrl], yUrl: tastes_params[:yUrl], yID: tastes_params[:yID])
+		
+		if @taste.save
+			currentUser = User.find(tastes_params[:userID])
+
+			if currentUser.tastes.any?{|taste| taste['name'] === @taste['name']}
+			render json: @taste, status: :ok
+
+			else
+				@taste.users << currentUser
+				render json: @taste, status: :created
+			end
+
 		else
-			render json: @tastes.errors.full_messages, status: :unprocessable_entity
+			render json: @taste.errors.full_messages, status: :unprocessable_entity
 		end
 	end
 
 	private
 
 	def tastes_params
-		params.require(:tastes).permit(:name, :type, :wTeaser, :wUrl, :yUrl, :yID)
+		params.permit(:name, :type, :teaser, :wUrl, :yUrl, :yID, :userID)
 	end
 
 end
